cb44ecf97737f5bd3089a826194f3eef6c1c0dfa
rohit <rohitchouhan3512@gmail.com>
2023-05-31 03:16:43 +0530

    created simple email service

---
+++ src/main/java/com/arjuncodes/springemaildemo/EmailSenderService.java
@@ -0,0 +1,34 @@
+package com.arjuncodes.springemaildemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@Service
+public class EmailSenderService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleEmail(String toEmail,
+                                String subject,
+                                String weatherDataContent
+    ) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("rohit.captain1509@gmail.com");
+        message.setTo(toEmail);
+        message.setText(weatherDataContent);
+        message.setSubject(subject);
+        mailSender.send(message);
+        System.out.println("Sent Successfully...");
+
+
+    }
+
+    }
